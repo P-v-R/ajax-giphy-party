10ddfbbf853ac1947f275467f76e00b7b97c5b5a
Perryvon <Perry.j.vr@gmail.com>
2021-04-13 18:36:31 -0700

    slight syntax tweaks

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ console.log("Let's get this party started!");
 let $submitBtn = $("#submit-btn");
 
 // utilizes axios to call giffy  api and return array of GIFs realted to user input and chooses first GIF to append to HTML body
-async function getGif(userInput) {
+async function getGif() {
     let userInput = $("#user-input").val();
     let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
     let imageUrl = gif.data.data[0].images.original.url;
     $("#gif-area").append(`<img src="${imageUrl}"/>`);
     console.log(userInput, `<img src="${gif.data.data[0].url}"/>`);
-    return imageUrl
+    return imageUrl;
 }
 
 // when form is submitted, calls getGif function. 
@@ -23,4 +23,4 @@ $submitBtn.on("click", async function (e) {
 // when reset button is clicked, reload page.
 $("#reset").on("click", function () {
     location.reload();
-})
+});
